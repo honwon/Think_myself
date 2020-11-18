3b8e831cb7f4c82f5d2ae6379be98fd0a109e7af
honwon <moheom98@gmail.com>
2020-11-18 18:28:28 +0900

    mouse

---
+++ fastcampusDown.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+
+while True:
+
+    # 내려받기 클릭
+    pyautogui.click(1149,181)
+    time.sleep(1.5)
+    
+    # late download 클릭
+    pyautogui.click(822,638)
+
+    # 다음 강의 클릭
+    pyautogui.click(140,752)
+    time.sleep(3)
